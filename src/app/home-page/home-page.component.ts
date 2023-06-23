e078426d3331e4c76525c9c62dcc129897a9c03c
HoangHoa99 <hoangthaihoa2122@gmail.com>
2023-06-23 16:26:28 +0700

    init home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  selectedSection: any;
+  greetingMessage: string = 'Bo';
+  interval: any;
+
+  ngOnInit() {
+    this.sectionSelected('about');
+
+    this.typingGreeting();
+  }
+
+  resetSection() {
+    this.selectedSection = {
+      about: false,
+      experiences: false,
+      blog: false,
+      contact: false
+    }
+  }
+
+  sectionSelected(sectionName: string) {
+    this.resetSection();
+    this.selectedSection[sectionName] = true;
+  }
+
+  typingGreeting() {
+    let message = 'njour!';
+    let i = 0;
+    this.interval = setInterval(() => {
+      this.greetingMessage += message.charAt(i).toString();
+      i++;
+      if (this.greetingMessage.length == 8) {
+        clearInterval(this.interval);
+      }
+      console.log(this.greetingMessage);
+    }, 800);
+  }
 }
