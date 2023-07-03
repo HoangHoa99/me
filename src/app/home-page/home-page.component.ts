344b93c54e9ec91585f9df45be546f7786e78b62
HoangHoa99 <hoangthaihoa2122@gmail.com>
2023-07-03 12:57:07 +0700

    update cv

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -24,7 +24,7 @@ export class HomePageComponent {
     "INCEPTIONLABS": "https://inceptionlabs.com.vn/",
     "CITYNOW": "https://citynow.vn/home",
     "TANCA": "https://tanca.io/",
-    "CV_LINK": "https://drive.google.com/file/d/1wke2KdpiwpsvKDLI7yojAPJeDwsHsRB_/view?usp=sharing"
+    "CV_LINK": "https://drive.google.com/file/d/1hnKtiFDiHOt9Xv2IC0YPqNhWtjz0AlOg/view?usp=sharing"
   };
 
   ngOnInit() {
